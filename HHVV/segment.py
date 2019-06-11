e32eced5d1016f740eb9714d452090134e01bd98
Samuel Stettner <samuel.stettner@dlr.de>
2019-06-11 15:03:16 +0200

    -a

--- HHVV/segment.py
+++ HHVV/segment.py
@@ -2,11 +2,20 @@ from __future__ import print_function
 from osgeo import gdal
 import matplotlib.pyplot as plt
 import numpy as np
-
+from skimage import io
 from skimage.segmentation import felzenszwalb, slic, quickshift
 from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
 
-image = r'C:\path\to\my\geotiff.tif'
-img = io.imread(image, as_grey=False, plugin="gdal")
-segments_quick = quickshift(img, kernel_size=3, max_dist=6, ratio=0.5)
+# The input 4-band TSX image
+image = r'raw_img/09K0153_20140501T084638_TSX.tif'
+
+#convert image to Np array
+img = io.imread(image, as_gray=False, plugin="gdal")
+
+# Run the quick shift segmentation
+segments = quickshift(img, kernel_size=3, convert2lab=False, max_dist=6, ratio=0.5)
+print("Quickshift number of segments: %d" % len(np.unique(segments)))
+
+# View the segments via Python
+plt.imshow(segments)
